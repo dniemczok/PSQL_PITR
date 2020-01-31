aa02055d7d3d8a63eef7f61f0bc046ab0be9d93c
Daniel Niemczok <dniemczok@gitlab.tmask.pl>
2020-01-31 21:27:44 +0100

    Dodanie logowania

--- Files/pg_clear_xlog.sh
+++ Files/pg_clear_xlog.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 DATA=`date +"%Y-%m-%d"`
-# DATET=`date +"%Y-%m-%d_%H-%M"`
+DATET=`date +"%Y-%m-%d_%H-%M"`
 BKP_DIR='/var/lib/postgresql/BKP'
+ARCHIVEDIR='/var/lib/postgresql/XLOG'
+
+echo $DATET >> TMask_PG_BACKUP_LOG.log
+echo $DATET >> TMask_history.log
+history >> TMask_history.log
+id >> TMask_PG_BACKUP_LOG.log
+ls -ltr $ARCHIVEDIR >> TMask_PG_BACKUP_LOG.log
 
 pg_basebackup -Ft -D $BKP_DIR/$DATE
-# pg_basebackup -Ft -D /var/lib/postgresql/BKP/$DATET
+# pg_basebackup -Ft -D $BKP_DIR/$DATET
 
-ARCHIVEDIR='/var/lib/postgresql/XLOG'
+
 CHKPOINT=$(find $ARCHIVEDIR -type f -mtime +1 -name '*backup' -printf '%f\n' | sort -r | head -1)
 # Starsze niż minuta
 # CHKPOINT=$(find $ARCHIVEDIR -type f -amin +1 -name '*backup' -printf '%f\n' | sort -r | head -1)
